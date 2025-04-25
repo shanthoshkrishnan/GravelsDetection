e733f64abb076e31e0820af177b54a5be8306f1b
SHANTHOSHHH <sandykrishnan48@gmail.com>
2025-04-25 12:04:13 +0530

    Updated using render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -5,8 +5,11 @@ set -o errexit
 # Update pip
 pip install --upgrade pip wheel setuptools
 
-# Install pre-compiled numpy wheel directly
-pip install https://files.pythonhosted.org/packages/5a/50/19332b6d3f2033a16553f921fff692bb700ff8028c42865c935e1d56c7b2/numpy-1.22.4-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+# Install specific versions from PyPI using binary-only approach
+pip install --only-binary=:all: numpy==1.21.6
+
+# Install opencv-python-headless last
+pip install opencv-python-headless==4.5.3.56
 
 # Install the rest
 pip install -r requirements.txt
