22e51f16ac173267850d4e7a33f0ece4a53a0dc1
SHANTHOSHHH <sandykrishnan48@gmail.com>
2025-04-25 11:23:07 +0530

    Updated using render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Exit on error
+set -e
+
 # Ensure pip, setuptools, and wheel are up-to-date
 pip install --upgrade pip setuptools wheel
 
-
-# Install the necessary system libraries before pip install
-apt-get update && apt-get install -y libatlas-base-dev
-
 # Install the Python dependencies
 pip install -r requirements.txt
-pip install --upgrade numpy
