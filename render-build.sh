ffe17f07fd9c878492313cb9879cddd33d2e7aa9
SHANTHOSHHH <sandykrishnan48@gmail.com>
2025-04-25 11:30:36 +0530

    Updated using render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -5,5 +5,8 @@ set -e
 # Ensure pip, setuptools, and wheel are up-to-date
 pip install --upgrade pip setuptools wheel
 
-# Install the Python dependencies
+# Install NumPy first using binary package
+pip install numpy==1.24.3 --only-binary=numpy
+
+# Install the rest of the Python dependencies
 pip install -r requirements.txt
